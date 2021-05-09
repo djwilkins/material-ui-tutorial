2e986be5639462d337835a5a2fb459b4ce43ec2d
djwilkins <42594773+djwilkins@users.noreply.github.com>
2021-05-09 17:45:06 -0400

    Grid System - Actual Implementation on Notes
    We get rid of the practice content and apply the Material UI Grid system to our actual Notes. We also nest the whole thing in a Container component for a little margin.
    
    The styling still doesn't look great overall but the responsive distribution of elements for different screen sizes responsively is now in place.

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Container from '@material-ui/core/Container';
 
 export default function Notes() {
   const [notes, setNotes] = useState([]);
@@ -12,28 +13,17 @@ export default function Notes() {
   }, []);
 
   // For "xs" (extra small) size screens and up, each Grid item should take up 12 columns (in the 12 column grid)
-  // For "sm" (small) size screens and up, each Grid item below should take up 6 columns (in the 12 column grid)
-  // For "md" (medium) size screens and up, each Grid item below should take up 3 columns (in the 12 column grid)
+  // For "md" (medium) size screens and up, each Grid item below should take up 6 columns (in the 12 column grid)
+  // For "lg" (large) size screens and up, each Grid item below should take up 4 columns (in the 12 column grid)
   return (
-    <div>
+    <Container>
       <Grid container>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>1</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>2</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>3</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>4</Paper>
-        </Grid>
+        {notes.map(note => (
+          <Grid item key={note.id} xs={12} md={6} lg={4}>
+            <Paper>{ note.title }</Paper>
+          </Grid>
+        ))}
       </Grid>
-
-      {notes.map(note => (
-        <p key={note.id}>{ note.title }</p>
-      ))}
-    </div>
+    </Container>
   )
 }
