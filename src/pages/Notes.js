b289709785adf9eac0e3acc821ef875e955d1bd0
djwilkins <42594773+djwilkins@users.noreply.github.com>
2021-05-15 18:42:28 -0400

    Card Component - Prep Work
    Setting up a new NoteCard component to use in our existing Notes component, where we'll pass the note object as a prop.  For now, we just use that in NoteCard to display the note.title.
    
    Next, we'll utilize Material UI's Card component in NoteCard to flesh it out and style it up nice.

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Container from '@material-ui/core/Container';
+import NoteCard from '../components/NoteCard';
 
 export default function Notes() {
   const [notes, setNotes] = useState([]);
@@ -20,7 +21,7 @@ export default function Notes() {
       <Grid container>
         {notes.map(note => (
           <Grid item key={note.id} xs={12} md={6} lg={4}>
-            <Paper>{ note.title }</Paper>
+            <NoteCard note={note} />
           </Grid>
         ))}
       </Grid>
